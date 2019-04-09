bc5c46ac5c727f2043a8780ba8b0c8df476e2f5c
Kenny <kenny.hom27@gmail.com>
2019-04-09 16:23:14 -0400

    solved reverse string int and palindrome

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let str = parseInt(n.toString().split("").reverse().join(""))
+  
+  return n < 0 ? str * -1 : str
+}
 
 module.exports = reverseInt;
