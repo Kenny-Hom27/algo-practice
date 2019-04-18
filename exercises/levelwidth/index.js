d8ae23b4df6a16d9307b334d277bf25366006f74
Kenny <kenny.hom27@gmail.com>
2019-04-18 10:45:42 -0400

    solved tree problems

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const final = [0];
+
+  let arr = [root, "stop"];
+
+  while (arr.length > 1) {
+    let current = arr.shift();
+
+    if (current === "stop") {
+      final.push(0);
+      arr.push('stop')
+    } else {
+      arr.push(...current.children);
+      final[final.length - 1]++;
+    }
+    
+  }
+  return final;
+}
 
 module.exports = levelWidth;
